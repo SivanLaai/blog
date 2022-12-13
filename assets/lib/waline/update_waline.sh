58709281441c0317c1b09058691fdb42ffe25d61
SivanLaai <lyhhap@163.com>
2022-12-13 13:02:01 +0800

    add waline update shell

---
+++ assets/lib/waline/update_waline.sh
@@ -0,0 +1,5 @@
+mv waline.css waline.css.bak
+mv waline.js waline.js.bak
+wget https://cdn.jsdelivr.net/npm/@waline/client@latest/dist/waline.js
+wget https://cdn.jsdelivr.net/npm/@waline/client@latest/dist/waline.css
+rm -rf .bak
